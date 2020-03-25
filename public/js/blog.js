c0709ab5604c00093b08a276c41cdf0c9671ec2d
orobbio <53920540+orobbio@users.noreply.github.com>
2020-03-24 20:54:18 -0400

    revision 5

--- public/js/blog.js
+++ public/js/blog.js
@@ -109,7 +109,7 @@ $(document).ready(function() {
     blogContainer.empty();
     var messageH2 = $("<h2>");
     messageH2.css({ "text-align": "center", "margin-top": "50px" });
-    messageH2.html("No posts yet for this category, navigate <a href='/cms'>here</a> in order to create a new post.");
+    messageH2.html("No SOP's yet for this Applications, navigate <a href='/cms'>here</a> in order to create a new SOP.");
     blogContainer.append(messageH2);
   }
 

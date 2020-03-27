b0cc5c08a53149d56faeaaaa52459df2670172c5
orobbio <53920540+orobbio@users.noreply.github.com>
2020-03-26 22:05:08 -0400

    revision 8

---
+++ src/containers/component/ImageComponent.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles.css";
+export default class ImageComponent extends React.Component {
+  state = { isOpen: false };
+
+  handleShowDialog = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+    console.log("cliked");
+  };
+
+  render() {
+    return (
+      <div>
+        <img
+          className="small"
+          src="/li.png"
+          onClick={this.handleShowDialog}
+          alt="no image"
+        />
+        {this.state.isOpen && (
+          <dialog
+            className="dialog"
+            style={{ position: "absolute" }}
+            open
+            onClick={this.handleShowDialog}
+          >
+            <img
+              className="image"
+              src="/df.jpg"
+              onClick={this.handleShowDialog}
+              alt="no image"
+            />
+          </dialog>
+        )}
+      </div>
+    );
+  }
+}
--- src/containers/login.js
+++ src/containers/login.js
@@ -1,43 +1,16 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
-export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-
+import "./styles.css";
+import ImageComponent from "./component/ImageComponent";
+function App() {
   return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
+    <div className="App">
+      <h1>Modal popup</h1>
+      <h4>To trigger the popup click the picture, please! </h4>
+      <ImageComponent />
     </div>
   );
 }
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
